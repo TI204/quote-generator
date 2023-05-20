61aab33cf815a04d7ffd6bb24031368b4eee233d
Rado Rakotonarivo <rado.raktn@gmail.com>
2023-05-20 15:56:39 +0200

    Created main.py quotes.py

---
+++ main.py
@@ -0,0 +1,26 @@
+from quotes import *
+
+def menu():
+    print("\n==== Programming Quotes ====")
+    print("1. Random quote")
+    print("2. All quotes")
+    print("3. Exit")
+
+def main():
+    while True:
+        menu()
+
+        choice = input("Choos your an action (1-3): ")
+        
+        if choice == "1":
+            print_quote(random_quote())
+        elif choice == "2":
+            view_quotes()
+        elif choice == "3":
+            print("Good bye...")
+            break
+        else:
+            print("Invalid input")
+
+if __name__ == "__main__":
+    main()
